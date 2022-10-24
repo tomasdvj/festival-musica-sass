6822339b0926a834b944daaf664b9a9b1a8258d5
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-24 12:40:25 -0300

    Se movio la carpeta img a src para correcto funcionamiento de codigo

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,14 @@
 
 // exports.tarea = tarea; //! se llama a la funcion para luego ejecutarla con npx gulp <nombre de la funcion>
 
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+//! CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//! Imagenes
+const webp = require('gulp-webp');
 
 function css( done ) {
     
@@ -21,6 +26,18 @@ function css( done ) {
     done();
 }
 
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
 function dev( done ) {
 
     watch('src/scss/**/*.scss', css);
@@ -29,4 +46,5 @@ function dev( done ) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
